a36953725c5fc95dbc35510aa168cf9ed0bdcf2f
skbnawaz <skbnawaz3@gmail.com>
2026-06-10 17:47:08 +0530

    secret key length fixed

--- tests/saudeDemo_login.spec.ts
+++ tests/saudeDemo_login.spec.ts
@@ -5,12 +5,14 @@ import dotenv from "dotenv";
 
 test("sauce demo login", async ({ page }) => {
   const commonUtils = new CommonUtils();
-  /*   const enc_userName = commonUtils.encryptData("standard_user");
+  const enc_userName = commonUtils.encryptData("standard_user");
   console.log("username", enc_userName);
   const enc_password = commonUtils.encryptData("secret_sauce");
-  console.log("password", enc_password); */
-  const username = commonUtils.decryptData(process.env.USER_NAME!);
-  const password = commonUtils.decryptData(process.env.PASSWORD!);
+  console.log("password", enc_password);
+  const username = commonUtils.decryptData(enc_userName);
+  console.log(username);
+  const password = commonUtils.decryptData(enc_password);
+  console.log(password);
   await page.goto("https://www.saucedemo.com/");
   await page.locator('[data-test="username"]').fill(username);
   await page.locator('[data-test="password"]').fill(password);
